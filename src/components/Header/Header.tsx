9a8dbfefc22c6636ecfe49c31bc96982affe86fe
dmitbykanov <dmitbykanov@gmail.com>
2026-03-12 17:12:58 +0200

    add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import css from "./Header.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className={css.header}>
+      <Link href="/" aria-label="Home">
+        <Image
+          src="/Logo.svg"
+          alt="Rental Car Logo"
+          width={104}
+          height={16}
+          className={css.logo}
+        />
+      </Link>
+      <nav aria-label="Navigation">
+        <ul className={css.navigation}>
+          <li className={css.navigationItem}>
+            <Link href="/" className={css.navigationLink}>
+              Home
+            </Link>
+          </li>
+          <li className={css.navigationItem}>
+            <Link href="/catalog" className={css.navigationLink}>
+              Catalog
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
