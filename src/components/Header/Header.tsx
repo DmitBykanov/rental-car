482b4569b5ed15f589539caa2881eee3c11c81b5
dmitbykanov <dmitbykanov@gmail.com>
2026-03-13 10:35:20 +0200

    add active

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,32 @@
+"use client";
 import css from "./Header.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home" className={css.logo}>
-        <Image
-          src="/Logo.svg"
-          alt="Rental Car Logo"
-          width={104}
-          height={16}
-          className={css.logo}
-        />
+      <Link href="/" aria-label="Home">
+        <Image src="/Logo.svg" alt="Rental Car Logo" width={104} height={16} />
       </Link>
       <nav aria-label="Navigation">
         <ul className={css.navigation}>
           <li className={css.navigationItem}>
-            <Link href="/" className={css.navigationLink}>
+            <Link
+              href="/"
+              className={`${css.navigationLink} ${pathname === "/" ? css.active : ""}`}
+            >
               Home
             </Link>
           </li>
           <li className={css.navigationItem}>
-            <Link href="/catalog" className={css.navigationLink}>
+            <Link
+              href="/catalog"
+              className={`${css.navigationLink} ${pathname === "/catalog" ? css.active : ""}`}
+            >
               Catalog
             </Link>
           </li>
