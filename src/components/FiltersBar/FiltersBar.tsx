7e45999ad350d6d2c59b18014a5b63c264d8fe7a
dmitbykanov <dmitbykanov@gmail.com>
2026-03-17 09:16:08 +0200

    add reset btn

--- src/components/FiltersBar/FiltersBar.tsx
+++ src/components/FiltersBar/FiltersBar.tsx
@@ -67,146 +67,170 @@ export default function FiltersBar() {
         loadCars();
       }}
     >
-      {({ values, setFieldValue, errors, touched }) => (
-        <Form className={css.form}>
-          <div className={css.field} ref={brandRef}>
-            <p className={css.label}>Car brand</p>
-            <div
-              className={`${css.customSelect} ${css.brandWidth} ${isBrandOpen ? css.focus : ""}`}
-              onClick={() => {
-                setIsBrandOpen(!isBrandOpen);
-                setIsPriceOpen(false);
-              }}
-            >
-              <span
-                className={values.brand ? css.selectedText : css.placeholder}
+      {({ values, setFieldValue, resetForm, errors, touched }) => {
+        const handleReset = () => {
+          const emptyFilters = {
+            brand: "",
+            price: "",
+            minMileage: "",
+            maxMileage: "",
+          };
+          setFilters(emptyFilters);
+          resetSearch();
+          resetForm({ values: emptyFilters });
+          loadCars();
+        };
+
+        return (
+          <Form className={css.form}>
+            <div className={css.field} ref={brandRef}>
+              <p className={css.label}>Car brand</p>
+              <div
+                className={`${css.customSelect} ${css.brandWidth} ${isBrandOpen ? css.focus : ""}`}
+                onClick={() => {
+                  setIsBrandOpen(!isBrandOpen);
+                  setIsPriceOpen(false);
+                }}
               >
-                {values.brand || "Choose a brand"}
-              </span>
-              <svg className={css.icon} width="16" height="16">
-                <use
-                  href={
-                    isBrandOpen
-                      ? "/Icons.svg#chevron-Active"
-                      : "/Icons.svg#chevron -Default"
-                  }
-                />
-              </svg>
+                <span
+                  className={values.brand ? css.selectedText : css.placeholder}
+                >
+                  {values.brand || "Choose a brand"}
+                </span>
+                <svg className={css.icon} width="16" height="16">
+                  <use
+                    href={
+                      isBrandOpen
+                        ? "/Icons.svg#chevron-Active"
+                        : "/Icons.svg#chevron-Default"
+                    }
+                  />
+                </svg>
 
-              {isBrandOpen && (
-                <ul className={css.optionsList}>
-                  {brands.map((brand) => (
-                    <li
-                      key={brand}
-                      className={css.option}
-                      onClick={() => {
-                        setFieldValue("brand", brand);
-                        setIsBrandOpen(false);
-                      }}
-                    >
-                      {brand}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          </div>
-
-          <div className={css.field} ref={priceRef}>
-            <p className={css.label}>Price/ 1 hour</p>
-            <div
-              className={`${css.customSelect} ${css.priceWidth} ${isPriceOpen ? css.focus : ""}`}
-              onClick={() => {
-                setIsPriceOpen(!isPriceOpen);
-                setIsBrandOpen(false);
-              }}
-            >
-              <span
-                className={values.price ? css.selectedText : css.placeholder}
-              >
-                {values.price ? `To ${values.price}$` : "Choose a price"}
-              </span>
-              <svg className={css.icon} width="16" height="16">
-                <use
-                  href={
-                    isPriceOpen
-                      ? "/Icons.svg#chevron-Active"
-                      : "/Icons.svg#chevron -Default"
-                  }
-                />
-              </svg>
-
-              {isPriceOpen && (
-                <ul className={css.optionsList}>
-                  {priceOptions.map((p) => (
-                    <li
-                      key={p}
-                      className={css.option}
-                      onClick={() => {
-                        setFieldValue("price", p.toString());
-                        setIsPriceOpen(false);
-                      }}
-                    >
-                      {p}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          </div>
-
-          <div className={css.field}>
-            <p className={css.label}>Car mileage / km</p>
-            <div
-              className={`${css.mileageGroup} ${errors.maxMileage && touched.maxMileage ? css.errorBorder : ""}`}
-            >
-              <div className={css.inputBox}>
-                <span className={css.prefix}>From</span>
-                <Field
-                  name="minMileage"
-                  className={css.input}
-                  value={
-                    values.minMileage
-                      ? formatMileage(Number(values.minMileage))
-                      : ""
-                  }
-                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                    setFieldValue(
-                      "minMileage",
-                      e.target.value.replace(/\D/g, ""),
-                    )
-                  }
-                />
-              </div>
-              <div className={css.inputBox}>
-                <span className={css.prefix}>To</span>
-                <Field
-                  name="maxMileage"
-                  className={css.input}
-                  value={
-                    values.maxMileage
-                      ? formatMileage(Number(values.maxMileage))
-                      : ""
-                  }
-                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                    setFieldValue(
-                      "maxMileage",
-                      e.target.value.replace(/\D/g, ""),
-                    )
-                  }
-                />
+                {isBrandOpen && (
+                  <ul className={css.optionsList}>
+                    {brands.map((brand) => (
+                      <li
+                        key={brand}
+                        className={css.option}
+                        onClick={() => {
+                          setFieldValue("brand", brand);
+                          setIsBrandOpen(false);
+                        }}
+                      >
+                        {brand}
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </div>
             </div>
-            {errors.maxMileage && touched.maxMileage && (
-              <span className={css.errorText}>{errors.maxMileage}</span>
-            )}
-          </div>
 
-          <button type="submit" className={css.searchBtn}>
-            Search
-          </button>
-        </Form>
-      )}
+            <div className={css.field} ref={priceRef}>
+              <p className={css.label}>Price / 1 hour</p>
+              <div
+                className={`${css.customSelect} ${css.priceWidth} ${isPriceOpen ? css.focus : ""}`}
+                onClick={() => {
+                  setIsPriceOpen(!isPriceOpen);
+                  setIsBrandOpen(false);
+                }}
+              >
+                <span
+                  className={values.price ? css.selectedText : css.placeholder}
+                >
+                  {values.price ? `To ${values.price}$` : "Choose a price"}
+                </span>
+                <svg className={css.icon} width="16" height="16">
+                  <use
+                    href={
+                      isPriceOpen
+                        ? "/Icons.svg#chevron-Active"
+                        : "/Icons.svg#chevron-Default"
+                    }
+                  />
+                </svg>
+
+                {isPriceOpen && (
+                  <ul className={css.optionsList}>
+                    {priceOptions.map((p) => (
+                      <li
+                        key={p}
+                        className={css.option}
+                        onClick={() => {
+                          setFieldValue("price", p.toString());
+                          setIsPriceOpen(false);
+                        }}
+                      >
+                        {p}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            </div>
+
+            <div className={css.field}>
+              <p className={css.label}>Car mileage / km</p>
+              <div
+                className={`${css.mileageGroup} ${errors.maxMileage && touched.maxMileage ? css.errorBorder : ""}`}
+              >
+                <div className={css.inputBox}>
+                  <span className={css.prefix}>From</span>
+                  <Field
+                    name="minMileage"
+                    className={css.input}
+                    value={
+                      values.minMileage
+                        ? formatMileage(Number(values.minMileage))
+                        : ""
+                    }
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                      setFieldValue(
+                        "minMileage",
+                        e.target.value.replace(/\D/g, ""),
+                      )
+                    }
+                  />
+                </div>
+                <div className={css.inputBox}>
+                  <span className={css.prefix}>To</span>
+                  <Field
+                    name="maxMileage"
+                    className={css.input}
+                    value={
+                      values.maxMileage
+                        ? formatMileage(Number(values.maxMileage))
+                        : ""
+                    }
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                      setFieldValue(
+                        "maxMileage",
+                        e.target.value.replace(/\D/g, ""),
+                      )
+                    }
+                  />
+                </div>
+              </div>
+              {errors.maxMileage && touched.maxMileage && (
+                <span className={css.errorText}>{errors.maxMileage}</span>
+              )}
+            </div>
+
+            <div className={css.btnGroup}>
+              <button type="submit" className={css.searchBtn}>
+                Search
+              </button>
+              <button
+                type="button"
+                className={css.resetBtn}
+                onClick={handleReset}
+              >
+                Reset
+              </button>
+            </div>
+          </Form>
+        );
+      }}
     </Formik>
   );
 }
